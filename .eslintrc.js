14f903fc9754115bf5f8340d5cc7cfb0fa5f50c7
Rahim-Chan <chenrixi93@gmail.com>
2023-02-23 23:12:50 +0800

    chore: init

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,33 @@ module.exports = {
   rules: {
     '@typescript-eslint/prefer-interface': 0,
     '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
+    '@typescript-eslint/array-type': [
+      'error',
+      {
+        default: 'array-simple',
+      },
+    ],
     'no-return-assign': 0,
     semi: ['error', 'always'],
     'no-confusing-arrow': 0,
     'no-console': 0,
-    'max-len': ['error', { code: 120, ignoreComments: true, ignoreStrings: true }],
+    'max-len': [
+      'error',
+      {
+        code: 150,
+        ignoreComments: true,
+        ignoreStrings: true,
+      },
+    ],
     // see https://github.com/prettier/prettier/issues/3847
-    'space-before-function-paren': ['error', { anonymous: 'never', named: 'never', asyncArrow: 'always' }],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'always',
+      },
+    ],
     'no-underscore-dangle': 0,
     'no-plusplus': 0,
   },
